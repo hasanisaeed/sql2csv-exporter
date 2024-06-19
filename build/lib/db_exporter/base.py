e3726002046f7d1e5c52b5388f0915e1982c9515
Saeed <hassanisaeed19@gmail.com>
2024-06-19 13:18:10 +0330

    ignore build files

--- build/lib/db_exporter/base.py
+++
@@ -1,9 +0,0 @@
-class BaseDatabase:
-    def connect(self):
-        raise NotImplementedError
-
-    def get_tables(self):
-        raise NotImplementedError
-
-    def export_table_to_csv(self, table_name, output_dir):
-        raise NotImplementedError
